7da3fba9e90a4dd5cd1bf469d4c6556552540e1f
ErKristof <e99kristof@gmail,com>
2022-04-25 16:49:39 +0200

    controller hozzáadás

---
+++ Raktarkezeles.API/Controllers/RaktarkezelesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Raktarkezeles.API.Data;
+using Raktarkezeles.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Raktarkezeles.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RaktarkezelesController : ControllerBase
+    {
+        private readonly AlkatreszContext _context;
+
+        public RaktarkezelesController(AlkatreszContext context)
+        {
+            _context = context;
+        }
+        //[HttpGet]
+        //[Route("/Alkatreszek")]
+        //public async Task<ActionResult<List<int>>> GetAlkatreszIds()
+        //{
+
+        //}
+    }
+}
